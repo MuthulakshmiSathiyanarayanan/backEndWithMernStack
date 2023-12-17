6e0c5c0569d0fce44201176d1b1e2a80f7eef2cd
muthulakshmi <muthulakshmisathiyanarayanan5@gmail.com>
2023-12-17 18:46:58 +0530

    updating with put mtd

--- backEnd/index.js
+++ backEnd/index.js
@@ -49,6 +49,41 @@ app.get("/books", async (request, response) => {
   }
 });
 
+//route to get one book
+app.get("/books/:id", async (request, response) => {
+  try {
+    const { id } = request.params;
+    const book = await Book.findById(id);
+    return response.status(200).json(book);
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
+
+//route for update a book
+app.put("/books/:id", async (request, response) => {
+  try {
+    if (
+      !request.body.title ||
+      !request.body.author ||
+      !request.body.publishYear
+    ) {
+      return response.status(400).send({
+        message: "send all req fields:title,author,publishYear",
+      });
+    }
+    const { id } = request.params;
+    const result = await Book.findByIdAndUpdate(id, request.body);
+    if (!result) {
+      return response.status(404).json({ message: "book not found" });
+    }
+    return response.status(200).send({ message: "book updated successfully" });
+  } catch (error) {
+    console.log(error.message);
+    response.status(500).send({ message: error.message });
+  }
+});
 mongoose
   .connect(mongoDBurl)
   .then(() => {
